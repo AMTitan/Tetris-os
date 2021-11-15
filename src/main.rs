4e2c14e9bc93215c8354e2a0d450d644ce876e92
arthurmelton <amtitan@icloud.com>
2021-11-15 12:44:16 -0500

    print.rs

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,17 @@
 
 use core::panic::PanicInfo;
 
+mod print;
+use print::*;
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    print("Hello, World!".as_ref(), 0xf);
+    print("Hello, World!".as_ref(), 0xf, (10,5));
     loop {}
 }
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
     loop {}
-}
-
-fn print(stirng:&[u8], color:u8) {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in stirng.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = color;
-        }
-    }
 }
---
+++ src/print.rs
@@ -0,0 +1,14 @@
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+pub fn print(stirng:&[u8], color:u8, pos:(usize, usize)) {
+    let vga_buffer = 0xb8000 as *mut u8;
+    let mut offset = (pos.1*BUFFER_WIDTH)+pos.0;
+
+    for (i, &byte) in stirng.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset((i + offset) as isize * 2) = byte;
+            *vga_buffer.offset((i + offset) as isize * 2 + 1) = color;
+        }
+    }
+}
